bd4467a24df0a33179fa3d76e32dac6e56e40a11
Redwarx008 <redwarx008@outlook.com>
2023-08-22 18:18:18 +0800

    8.22

--- MipmapGen/main.cpp
+++ MipmapGen/main.cpp
@@ -2,157 +2,70 @@
 #include <vector>
 #include <string>
 #include <filesystem>
-#define STB_IMAGE_IMPLEMENTATION
-//#define STBI_ASSERT(x)
-#include "stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
 
-const float heightMultiplier = 100;
-
- stbi_inline static uint16_t get_channel_value(void* pixels, int bitDepth, 
-    int x, int y, int width, int height, int nChannel, int channel)
-{
-    if (x >= width) x = width - 1;
-    if (y >= height) y = height - 1;
-
-    uint16_t value;
-    if (bitDepth == 16)
-    {
-        //uint8_t high = pixels[(x + (size_t)y * width) * 2];
-        //uint8_t low = pixels[(x + (size_t)y * width) * 2 + 1];
-        //value = (high << 8) | low;
-
-        uint16_t* data = static_cast<uint16_t*>(pixels);
-        value = data[(x + (y * width)) * nChannel + channel];
-    }
-    else
-    {
-        uint8_t* data = static_cast<uint8_t*>(pixels);
-        value = data[(x + (y * width)) * nChannel + channel];
-    }
-     
-    return value;
-}
-
- stbi_inline static void set_channel_value(void* pixels, uint16_t value, int bitDepth,
-    int x, int y, int width, int nChannel, int channel)
-{
-    if (bitDepth == 16)
-    {
-        //uint8_t high = (value >> 8) & 0xff;
-        //uint8_t low = value & 0xff;
-        //pixels->at((x + (size_t)y * width) * 2) = high;
-        //pixels->at((x + (size_t)y * width) * 2 + 1) = low;
-
-        uint16_t* data = static_cast<uint16_t*>(pixels);
-        data[(x + (y * width)) * nChannel + channel] = value;
-    }
-    else
-    {
-        uint8_t* data = static_cast<uint8_t*>(pixels);
-        data[(x + (y * width)) * nChannel + channel] = value;
-    }
-}
-
- stbi_inline static std::string GetFileNameWithoutSuffix(const std::string& fileName)
- {
-     return fileName.substr(0, fileName.find_last_of('.'));
- }
-
- static void WriteAsTextureData(const std::string& fileName, void* pixels, int bitDepth, 
-     int nChannel, int height, int width)
- {
-     FILE* f;
-#if defined(_MSC_VER) && _MSC_VER >= 1400
-     if (0 != fopen_s(&f, fileName.c_str(), "ab+"))
-     {
-         return;
-     }
-#else 
-     f = fopen(fileName.c_str(), "ab+");
-     if (f == nullptr)
-     {
-         return;
-     }
-#endif // (_MSC_VER) && _MSC_VER >= 1400
-
-     uint8_t pixelInfo[] = {(uint8_t)bitDepth, (uint8_t)nChannel};
-     uint16_t sizeInfo[] = { (uint16_t)width, (uint16_t)height };
-     fwrite(&pixelInfo[0], sizeof(uint8_t), sizeof(pixelInfo) / sizeof(uint8_t), f);
-     fwrite(&sizeInfo[0], sizeof(uint16_t), sizeof(sizeInfo) / sizeof(uint16_t), f);
-
-     if (bitDepth == 16)
-     {
-         uint16_t* data = static_cast<uint16_t*>(pixels);
-         for (int y = 0; y < height; ++y)
-         {
-             for (int x = 0; x < width; ++x)
-             {
-                 for (int c = 0; c < nChannel; ++c)
-                 {
-                     uint32_t offset = (x + static_cast<size_t>(width) * y) * nChannel + c;
-                     fwrite(&data[offset], sizeof(float), 1, f);
-                 }
-             }
-         }
-         fclose(f);
-     }
-     else
-     {
-         fwrite(pixels, bitDepth / 8, static_cast<size_t>(width) * height * nChannel, f);
-         fclose(f);
-     }
- }
+#include "inicpp.h"
+#include "utility.h"
 
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
+    if (std::filesystem::exists("config.ini"))
     {
-        std::cout << "usage: [file] [max mip level].\n";
-        return 22;
+        ini::IniFile config;
+        config.load("config.ini");
+        Settings::FileName = config["args"]["FileName"].as<std::string>();
+        Settings::MaxMipLevel = config["args"]["MaxMipLevel"].as<int>();
+        Settings::ExportPng = config["settings"]["ExportPng"].as<bool>();
+        Settings::HeightMultiplier = config["settings"]["HeightMultiplier"].as<int>();
+        Settings::GenerateNormalMap = config["settings"]["GenerateNormalmap"].as<bool>();
     }
-    
-    int maxMipLevel = atoi(argv[2]);
+    else
+    {
+        if (argc != 3)
+        {
+            std::cout << "usage: [file] [max mip level].\n";
+            return 22;
+        }
+        Settings::MaxMipLevel = atoi(argv[2]);
+        Settings::FileName = argv[1];
+    }
 
-    std::string fileName = argv[1];
 
     int width, height, nChannel;
-    int ok = stbi_info(fileName.c_str(), &width, &height, &nChannel);
+    int ok = stbi_info(Settings::FileName.c_str(), &width, &height, &nChannel);
     if (ok == 0)
     {
         std::cout << stbi_failure_reason() << std::endl;
         return 3;
     }
 
-    bool is16Bit = stbi_is_16_bit(fileName.c_str()) == 1 ? true : false;
+    bool is16Bit = stbi_is_16_bit(Settings::FileName.c_str()) == 1 ? true : false;
     int bitDepth = is16Bit ? 16 : 8;
 
     void* originPixels;
     if (is16Bit)
     {
-        originPixels = stbi_load_16(fileName.c_str(), &width, &height, &nChannel, 0);
+        originPixels = stbi_load_16(Settings::FileName.c_str(), &width, &height, &nChannel, 0);
     }
     else
     {
-        originPixels = stbi_load(fileName.c_str(), &width, &height, &nChannel, 0);
+        originPixels = stbi_load(Settings::FileName.c_str(), &width, &height, &nChannel, 0);
     }
 
-    if (maxMipLevel < 1 || maxMipLevel > std::max(1, (int)std::log2(std::min(width, height))))
+    if (Settings::MaxMipLevel < 1 || Settings::MaxMipLevel > std::max(1, (int)std::log2(std::min(width, height))))
     {
         std::cout << "mip level is out of range.\n";
         return 2;
     }
 
-    std::string outputFileName = GetFileNameWithoutSuffix(fileName);
+    std::string outputFileName = GetFileNameWithoutSuffix(Settings::FileName);
     if (std::filesystem::exists(outputFileName))
     {
         std::filesystem::remove(outputFileName);
     }
     
     void* inPixels = originPixels;
-    for (int mip = 1; mip <= maxMipLevel; ++mip)
+    for (int mip = 1; mip <= Settings::MaxMipLevel; ++mip)
     {
         int h = floor((float)height / 2);
         int w = floor((float)width / 2);
@@ -211,8 +124,16 @@ int main(int argc, char* argv[])
                 }
             }
         }
-        /*stbi_write_png((GetFileNameWithoutSuffix(fileName) + '_' + std::to_string(mip) + ".png").c_str(), w, h, nChannel, outPixels, w);*/
-        WriteAsTextureData(outputFileName, outPixels, bitDepth, nChannel, h, w);
+
+        if (Settings::ExportPng && bitDepth == 8)
+        {
+            stbi_write_png((outputFileName + '_' + std::to_string(mip) + ".png").c_str(), w, h, nChannel, outPixels, w);
+        }
+        else
+        {
+            WriteTexture(outputFileName, outPixels, bitDepth, nChannel, h, w);
+        }
+
         stbi_image_free(inPixels);
         inPixels = outPixels;
         width = w;
--- MipmapGen/utility.h
+++ MipmapGen/utility.h
@@ -1,5 +1,12 @@
 #include <cstdint>
 #include <cmath>
+#include <string>
+
+#define STB_IMAGE_IMPLEMENTATION
+//#define STBI_ASSERT(x)
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 struct Vector3
 {
@@ -29,12 +36,130 @@ struct Vector3
     }
 };
 
-uint32_t* GenerateHeightmap()
+struct Settings
 {
+    static std::string FileName;
 
+    static int MaxMipLevel;
+
+    static float HeightMultiplier;
+
+    static bool GenerateNormalMap;
+
+    static bool ExportPng;
+};
+
+stbi_inline uint16_t get_channel_value(void* pixels, int bitDepth,
+    int x, int y, int width, int height, int nChannel, int channel)
+{
+    if (x >= width) x = width - 1;
+    if (y >= height) y = height - 1;
+
+    uint16_t value;
+    if (bitDepth == 16)
+    {
+        //uint8_t high = pixels[(x + (size_t)y * width) * 2];
+        //uint8_t low = pixels[(x + (size_t)y * width) * 2 + 1];
+        //value = (high << 8) | low;
+
+        uint16_t* data = static_cast<uint16_t*>(pixels);
+        value = data[(x + (y * width)) * nChannel + channel];
+    }
+    else
+    {
+        uint8_t* data = static_cast<uint8_t*>(pixels);
+        value = data[(x + (y * width)) * nChannel + channel];
+    }
+
+    return value;
 }
 
-uint16_t* GenerateNormalmap(float* heightmap, int width, int height)
+stbi_inline void set_channel_value(void* pixels, uint16_t value, int bitDepth,
+    int x, int y, int width, int nChannel, int channel)
+{
+    if (bitDepth == 16)
+    {
+        //uint8_t high = (value >> 8) & 0xff;
+        //uint8_t low = value & 0xff;
+        //pixels->at((x + (size_t)y * width) * 2) = high;
+        //pixels->at((x + (size_t)y * width) * 2 + 1) = low;
+
+        uint16_t* data = static_cast<uint16_t*>(pixels);
+        data[(x + (y * width)) * nChannel + channel] = value;
+    }
+    else
+    {
+        uint8_t* data = static_cast<uint8_t*>(pixels);
+        data[(x + (y * width)) * nChannel + channel] = value;
+    }
+}
+
+
+stbi_inline std::string GetFileNameWithoutSuffix(const std::string& fileName)
+{
+    return fileName.substr(0, fileName.find_last_of('.'));
+}
+
+void WriteTexture(const std::string& fileName, void* pixels, int bitDepth,
+    int nChannel, int height, int width)
+{
+    FILE* f;
+#if defined(_MSC_VER) && _MSC_VER >= 1400
+    if (0 != fopen_s(&f, fileName.c_str(), "ab+"))
+    {
+        return;
+    }
+#else 
+    f = fopen(fileName.c_str(), "ab+");
+    if (f == nullptr)
+    {
+        return;
+    }
+#endif // (_MSC_VER) && _MSC_VER >= 1400
+
+    uint8_t pixelInfo[] = { (uint8_t)bitDepth, (uint8_t)nChannel };
+    uint16_t sizeInfo[] = { (uint16_t)width, (uint16_t)height };
+    fwrite(&pixelInfo[0], sizeof(uint8_t), sizeof(pixelInfo) / sizeof(uint8_t), f);
+    fwrite(&sizeInfo[0], sizeof(uint16_t), sizeof(sizeInfo) / sizeof(uint16_t), f);
+
+    if (bitDepth == 16)
+    {
+        uint16_t* data = static_cast<uint16_t*>(pixels);
+        // for heightmap
+        if (nChannel == 1)
+        {
+            ExportHeightmap(f, data, width, height);
+        }
+        else
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    for (int c = 0; c < nChannel; ++c)
+                    {
+                        uint32_t offset = (x + static_cast<size_t>(width) * y) * nChannel + c;
+                        fwrite(&data[offset], sizeof(float), 1, f);
+                    }
+                }
+            }
+        }
+
+        fclose(f);
+    }
+    else
+    {
+        fwrite(pixels, bitDepth / 8, static_cast<size_t>(width) * height * nChannel, f);
+        fclose(f);
+    }
+}
+
+void ExportHeightmap(FILE* f, uint16_t* source, int width, int height)
+{
+    
+}
+
+uint8_t* GenerateNormalmap(float* heightmap, int width, int height)
 {
 	if (!heightmap)
 	{
@@ -42,7 +167,7 @@ uint16_t* GenerateNormalmap(float* heightmap, int width, int height)
 	}
 
 	uint32_t resolution = width * height;
-	uint16_t* normalmap = new uint16_t[resolution * 2]; //R16G16 texture
+    uint8_t* normalmap = new uint8_t[resolution * 2]; //R16G16 texture
 
 	for (int index = 0; index < resolution; ++index)
 	{
@@ -58,8 +183,8 @@ uint16_t* GenerateNormalmap(float* heightmap, int width, int height)
         Vector3 normal = { left - right, up - down, 2.0f };
         normal = normal.Normalized();
 
-        uint16_t x = ((normal.x + 1) / 2) * 65535;
-        uint16_t y = ((normal.y + 1) / 2) * 65535;
+        uint8_t x = ((normal.x + 1) / 2) * 255;
+        uint8_t y = ((normal.y + 1) / 2) * 255;
 
         normalmap[index * 2] = x;
         normalmap[index * 2 + 1] = y;
